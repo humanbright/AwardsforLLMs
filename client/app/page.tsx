d3323a415b16b77874b456f1051b328277101bc4
humanbright <humanbright529@gmail.com>
2024-07-31 12:13:21 -0400

    search component

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,10 +1,14 @@
 import Image from "next/image";
+import Search from "./components/search";
 
 export default function Home() {
   return (
-   <div className="flex h-screen items-center justify-center">
-    <Image src={'/logo.svg'} alt="NSF Logo" />
-      <h1>AWARDS</h1>
-   </div>
+    <div className=" h-screen p-12">
+      <Image src={"/logo.svg"} alt="NSF Logo" height={80} width={300} />
+      <div className="flex items-center justify-center mt-32 flex-col">
+        <h1 className=" font-bold text-7xl">Search Awards</h1>
+        <Search />
+      </div>
+    </div>
   );
 }
